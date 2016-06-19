ced374eb75cc08d23bab790804e9fc4e99d2d1d2
Mahmoud Saada <mahmoud@targetcw.com>
2016-06-19 16:59:12 -0400

    add example and update README

--- example.js
+++ example.js
@@ -1,8 +1,3 @@
-# download-image.js
-Library with simple API to download an image from a URL to a desired location
-
-```js
 let downloadImage = require('./download-image')
 downloadImage('http://lorempixel.com/g/400/200/', `./image.jpg`)
 downloadImage('lorempixel.com/g/400/200/', `./image2.jpg`)
-```
